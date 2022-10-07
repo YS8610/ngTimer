2fd368e78e9b87e92100e4c45307a9e17f0e1050
YS_workPC <ong_ys86@yahoo.com.sg>
2022-10-07 12:02:20 +0800

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   title = 'ngTimer';
+  counter = 0;
+  timeInput = 60;
+  time2 !: number;
+  timeCounter : Subscription;
+
+  constructor(){
+    this.timeCounter = Subscription.EMPTY;
+  }
+
+  ngOnDestroy(): void {
+    this.timeCounter.unsubscribe();
+  }
+
+  updateChange(event:Event){
+    const change = event.target;
+    this.timeInput = (change as HTMLInputElement).valueAsNumber
+  }
+
+
+  startTimer(){
+    this.timeCounter.unsubscribe();
+    this.time2 = this.timeInput;
+    this.timeCounter = interval(1000).subscribe( x =>{
+      if (this.time2>0){
+        this.time2--;
+      }
+      else {
+        this.timeCounter.unsubscribe();
+      }
+    })
+  }
+
+  resetTimer(){
+    this.timeCounter.unsubscribe();
+    this.time2 = this.timeInput;
+  }
+
+
+  addCounter(){
+    this.counter++;
+  }
+
+  minusCounter(){
+    if (this.counter>0){
+      this.counter--;
+    }
+  }
 }
